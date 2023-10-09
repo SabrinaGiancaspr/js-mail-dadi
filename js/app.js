768371a3895c6e4efa187edce35365b5bb8914ba
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-09 16:26:42 +0200

    edit readme add ciclo for

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 // #mail
 
 // -creare un array con dentro le graffe quadre []
-const mail = ["john123@email.com"]
-//   -assegnare variabile 
-//   -dichiarare variabile con "mail"
-//   -aprire parentesi quadre
-//     -dentro le quadre scrivo tot mail
+//-assegnare variabile 
+//-dichiarare variabile con "mail"
+//-aprire parentesi quadre
+const mail = ["john123@email.com", "sarah456@email.com", "mike789@email.com"]; //-dentro le quadre scrivo tot mail
+console.log(mail)
 
-
+const arrayMail = mail.length;
 // -creo un prompt per chiedere al utente la sua mail
-//   -assegnare variabile
-//   -dichiarare variabile per chiedere la mail
+//-assegnare variabile
+//-dichiarare variabile per chiedere la mail
+const userMail = prompt('Qual è la tua mail?');
+console.log(userMail);
 
+// -creare ciclo for per verificare se la mail 
+// del utente è uguale a uno degli indirizzi email nell'array
+
+for (let i = 0; i < mail.length; i++) {
+  if (userMail === mail[i]) { }
+}
 // -condizionali
-//   -SE la mail dell'utente rientra nella lista del array 
-//     -stampa messaggio positivo
-//   -ALTRIMENTI SE la mail non esiste nel array 
-//     -stampa messaggio negativo
+if (userMail === mail) {//-SE la mail dell'utente rientra nella lista del array 
+  console.log('si');//-stampa messaggio positivo
+
+} else if (userMail !== mail) {//-ALTRIMENTI SE la mail non esiste nel array 
+  console.log('no');//-stampa messaggio negativo
+}
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -6,11 +6,12 @@
   -aprire parentesi quadre
     -dentro le quadre scrivo tot mail
 
-
 -creo un prompt per chiedere al utente la sua mail
   -assegnare variabile
   -dichiarare variabile per chiedere la mail
 
+-creare ciclo for per verificare se la mail del utente è uguale a uno degli indirizzi email nell'array
+  
 -condizionali
   -SE la mail dell'utente rientra nella lista del array 
     -stampa messaggio positivo
