45e67592553a852ac3ff097328126f57d0a194dd
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-09 17:49:16 +0200

    edit js

--- js/app.js
+++ js/app.js
@@ -4,29 +4,33 @@
 //-assegnare variabile 
 //-dichiarare variabile con "mail"
 //-aprire parentesi quadre
-const mail = ["john123@email.com", "sarah456@email.com", "mike789@email.com"]; //-dentro le quadre scrivo tot mail
-console.log(mail)
+const arrayMails = ["john123@email.com", "sarah456@email.com", "mike789@email.com"]; //-dentro le quadre scrivo tot mail
+console.log(arrayMails);
 
-const arrayMail = mail.length;
+
 // -creo un prompt per chiedere al utente la sua mail
 //-assegnare variabile
 //-dichiarare variabile per chiedere la mail
 const userMail = prompt('Qual è la tua mail?');
 console.log(userMail);
 
+const listArrayMails = arrayMails.length;
+
+let permission = 'no';
+
 // -creare ciclo for per verificare se la mail 
 // del utente è uguale a uno degli indirizzi email nell'array
-
-for (let i = 0; i < mail.length; i++) {
-  if (userMail === mail[i]) { }
+for (let i = 0; i < listArrayMails; i++) {
+  let currentMail = arrayMails[i];
+  console.log(currentMail);
+  // -condizionali
+  if (userMail === currentMail) {//-SE la mail dell'utente è uguale a quella che sto scorrendo
+    permission = 'si';
+    // console.log(permission);//-stampa messaggio positivo
+  }
 }
-// -condizionali
-if (userMail === mail) {//-SE la mail dell'utente rientra nella lista del array 
-  console.log('si');//-stampa messaggio positivo
 
-} else if (userMail !== mail) {//-ALTRIMENTI SE la mail non esiste nel array 
-  console.log('no');//-stampa messaggio negativo
-}
+console.log(permission);
 
 
 
